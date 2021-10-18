85b9bbf3a162e015c788a29adde05db97298c0dc
Francisc Moldovan <franciscmoldovan93@gmail.com>
2021-10-18 21:32:51 +0300

    a very rough first shape

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ChatUser, TypedMessage, GroupChat
 # Register your models here.
+admin.site.register(ChatUser)
+admin.site.register(TypedMessage)
+admin.site.register(GroupChat)
--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
 
-# Create your models here.
+
+class ChatUser(models.Model):
+    username = models.CharField(null=False, unique=True, max_length=200)
+    email_address = models.CharField(null=False, max_length=200)
+    django_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class GroupChat(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class TypedMessage(models.Model):
+    contents = models.CharField(max_length=1000)
+    owner = models.ForeignKey(ChatUser, related_name="mymessage", on_delete=models.CASCADE)
+    chat = models.ForeignKey(GroupChat, related_name="chat_from", on_delete=models.CASCADE)
